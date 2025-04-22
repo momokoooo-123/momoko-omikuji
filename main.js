831f59551247f7ca85accff40eaf86cb1f91a8f8
momokoooo-123 <satouma.aaaaa@gmail.com>
2025-04-23 00:26:16 +0900

    Update main.js

--- main.js
+++ main.js
@@ -13,22 +13,8 @@ function drawOmikuji() {
     resultImg.src = result;
     resultImg.style.display = 'block';
   }, 2000);
-  const againBtn = document.getElementById("againBtn");
-
-againBtn.addEventListener("click", () => {
-  // 初期状態に戻す
-  resultImg.style.display = "none";
-  againBtn.style.display = "none";
-  tapBtn.style.display = "block";
-  title.style.display = "block";
+  document.getElementById("again-btn").style.display = "block";
+  document.getElementById("again-btn").addEventListener("click", function () {
+  location.reload();
 });
-
-// 結果を表示したあとに、再抽選ボタンを表示
-function showResult(again.png) {
-  resultImg.src = result;
-  resultImg.style.display = "block";
-  tapBtn.style.display = "none";
-  title.style.display = "none";
-  againBtn.style.display = "block";
-}
 }
