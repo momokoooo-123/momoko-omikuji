f1a36ab67bdd79d25ad4db06cdd3be4ac1f64edb
momokoooo-123 <satouma.aaaaa@gmail.com>
2025-04-22 12:42:41 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,13 @@ function drawOmikuji() {
   document.getElementById('startImg').style.display = 'none';
   document.getElementById('lotteryImg').style.display = 'block';
 
-  // 抽選のように見せるために2秒待つ
   setTimeout(function() {
     document.getElementById('lotteryImg').style.display = 'none';
 
-    // 結果のパターンをランダムに選ぶ
-    const results = ['daikichi.gif', 'kichi.gif', 'kyou.gif'];
+    const results = ['daikichi.png', 'chukichi.png', 'shoukichi.png', 'kyou.png'];
+
     const result = results[Math.floor(Math.random() * results.length)];
 
-    // 結果画像の表示
     const resultImg = document.getElementById('resultImg');
     resultImg.src = result;
     resultImg.style.display = 'block';
